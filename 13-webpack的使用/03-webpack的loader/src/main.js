ede9b82dd8cafab10c9345bda1b7bdd75eeb0b41
sun198986 <334828328@qq.com>
2020-08-10 02:44:47 +0800

    前端模块化和组件化高级语法

---
+++ 13-webpack的使用/03-webpack的loader/src/main.js
@@ -0,0 +1,14 @@
+//使用commonjs的模块化规范
+const {add,mul} = require('./js/mathUtils.js')
+
+console.log(add(20,30));
+console.log(mul(20,4));
+
+//使用es6的模块化规范
+import {name,age,height} from "./js/info";
+
+console.log(name);
+console.log(age);
+console.log(height);
+
+//webpack4打包 webpack ./src/main.js -o ./dist/bundle.js
